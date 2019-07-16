1fed6be24ea7f001320d5b4b1a122fdbf20d445d
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-16 17:03:13 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle
 
 attr_accessor :wheel_size, :wheel_number
 
-  def initailaize (wheel_size, wheel_number)
+  def initialize (wheel_size, wheel_number)
     @wheel_size =  wheel_size
     @wheel_number = wheel_number
   end
